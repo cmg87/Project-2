7edbfd9ae44a01f8d976bcf1394f5a1f69481007
Christian Saxton <CRS-MACBOOK@CRS-MBP.local>
2018-06-30 11:53:31 -0400

    Added React structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './components/NavBar';
+
+
+class App extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default App;
