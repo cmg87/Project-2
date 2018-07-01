b979908c0ef599166350d9e28de10da04b1749f5
Christian Saxton <CRS-MACBOOK@CRS-MBP.local>
2018-07-01 15:24:18 -0400

    Daily push. Began structuring SurveyList functionality.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './containers/Home';
 
 
 class App extends Component {
   render() {
     return (
-      <NavBar />
+      <div>
+        <NavBar />
+        <Home />
+      </div>
     );
   }
 }
---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import SurveyList from '../components/SurveyList';
+
+class Home extends Component {
+    render() {
+        return (
+            <div id="homeContainer" className="container">
+                <h1>Welcome to Kwiri!</h1>
+                <h2>Please select a survey from the list below:</h2>
+                <hr />
+                <SurveyList />
+            </div>
+        );
+    }
+}
+
+export default Home;
