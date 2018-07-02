f8d1c65507e7be4f8566926c81c587a115da7408
Christian Saxton <CRS-MACBOOK@CRS-MBP.local>
2018-07-02 12:54:28 -0400

    Added React router for HTML routing. Updated AJAX called to correctly fill survey information on home page. Updated package.json with proxy to coordinate calls between React and Express.

---
+++ src/containers/Survey.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Survey extends Component {
+    render() {
+        return (
+            <div id="surveyContainer" className="container">
+                survey placeholder
+            </div>
+        );
+    }
+}
+
+export default Survey;
