0542149329f89ba5505e9b029e05f8845ed1d184
Unknown <saxton.christian@gmail.com>
2018-07-06 21:01:55 -0400

    Auto stash before merge of "master" and "origin/master"
    Wrote filter for results page

--- src/components/Results/Result.js
+++ src/components/Results/Result.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import ParseRes from './ParseRes';
+import Filter from './Filter';
 
 
 class Result extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            response: null
+            response: null,
+            filterData: null
         };
+        this.filterState = this.filterState.bind(this);
         this.surveykey = require('../../data/surveys/survey1answerkey.json');
     }
 
     componentWillMount() {
+        if (this.state.filterData !== null) {
+            fetch('/api/getResults', {
+                method: 'POST',
+                body: this.state.filterData
+            }).then(data =>{
+                console.log(data);
+                this.setState({response: data[0]});
+            });
+        }
+        
         fetch('/api/estResults')
         .then(results => {
             return results.json();
         }).then(data => {
             console.log(data);
             this.setState({response: data[0]});
-        })
+        });
+    }
+
+    filterState(filterData) {
+        this.setState({
+            filterData: filterData
+        });
     }
 
     render() {
@@ -32,7 +51,10 @@ class Result extends Component {
             );
         }
         return(
+            <div>
+            <Filter filterState={this.filterState}/>
             <ParseRes survey={this.surveykey} answers={this.props.answers} response={this.state.response} />
+            </div>
         )
     }
 }
